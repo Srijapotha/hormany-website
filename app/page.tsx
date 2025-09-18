4ce229d9fccf4449ece4f2c7012c0fc2c6cf06cc
Somya Singh Gour <somya.gour@baseel.com>
2025-09-19 03:47:38 +0530

    Home page development started

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,82 @@
+import { Banner, Map } from "@/assets";
+import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+import { ChartNoAxesCombined, Eye } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <>
+      <div className="relative bg-black">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src={Banner}
+          height={400}
+          width={1400} // Keep this in proportion to the actual image
+          priority // Loads this image as a high-priority image (above-the-fold)
+          alt="Advancing Human Rights, Equality, and Diversity"
+          className="w-full h-auto object-cover lg:container lg:max-w-8xl mx-auto" // Ensures the image covers its container without stretching
+          layout="responsive" // Ensures the image maintains its aspect ratio while scaling
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+        <div className="absolute inset-0 items-center justify-center px-5 md:px-16 text-white lg:container lg:max-w-7xl mx-auto">
+          <div>
+            <h1 className="leading-normal text-white text-3xl font-semibold ">
+              Advancing Human Rights, Equality, and Diversity
+            </h1>
+            <p className="mt-5 text-sm hidden md:block !leading-loose text-justify">
+              Together we work to build harmony, support those in need, and create inclusive communities through education, citizenship, and collaboration
+            </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            {/* <Link href="https://app.maulee.org/" target="_blank">
+                            <Button className="h-auto bg-[linear-gradient(95deg,_#F05623_0%,_#9B1B37_80.26%)] rounded-full mt-4 transform transition-transform duration-300 ease-in-out hover:scale-105 relative">
+                                Find a Charity <MoveRight className="ml-2" />
+                            </Button>
+                        </Link> */}
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+
+      <div className="lg:container lg:max-w-7xl mx-auto px-6 py-10">
+        <h2 className="text-3xl font-semibold text-center">Vision & Mission</h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 py-8">
+          <div className="h-full">
+            <Card className="h-full flex flex-col p-10 border-l-4 border-[#2E6BE6] border-t-0 border-r-0 border-b-0 shadow-lg">
+              <CardTitle className="text-xl">Our Vision</CardTitle>
+              <span className="bg-[#2E6BE6] rounded-full p-4 w-fit my-4">
+                <Eye className="text-white" />
+              </span>
+              <CardDescription className="text-justify mt-auto">
+                To build a just, inclusive, and peaceful world
+                where diversity is celebrated, human rights are upheld, and every individual has the freedom to live with dignity, equality, and mutual respect in a thriving global community.
+              </CardDescription>
+            </Card>
+          </div>
+
+          <div className="h-full">
+            <Card className="h-full flex flex-col p-10 border-l-4 border-[#7A60D1] border-t-0 border-r-0 border-b-0 shadow-lg">
+              <CardTitle className="text-xl">Our Mission</CardTitle>
+              <span className="bg-[#7A60D1] rounded-full p-4 w-fit my-4">
+                <ChartNoAxesCombined className="text-white" />
+              </span>
+              <CardDescription className="text-justify mt-auto">
+                To challenge hate, prejudice, and discrimination everywhere by supporting victims with care and relief services, promoting dialogue across faiths and cultures, and empowering people through education, advocacy, and unity.
+              </CardDescription>
+            </Card>
+          </div>
+        </div>
+
+
+      </div>
+
+      <div className="bg-[#0F172A]">
+        <div className="text-white text-center lg:container lg:max-w-7xl mx-auto px-6 py-10">
+
+          <h3 className="text-3xl font-semibold">Global Impact & Partners</h3>
+          <p>Working with leading organizations worldwide to create lasting change.</p>
+
+          <Image src={Map} alt="" className="p-5 md:p-15" />
+        </div>
+      </div>
+
+    </>
   );
 }
---
+++ assets/index.ts
@@ -0,0 +1,8 @@
+import Banner from "./images/banner.webp";
+import Map from "./images/map.webp";
+
+export{
+    Banner,
+    Map,
+
+ };
